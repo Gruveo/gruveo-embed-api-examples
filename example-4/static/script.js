9d8a479a1e31b1876474f84ea24cee2f4d5db2a0
Martin Ždila <martin@gruveo.com>
2017-07-07 10:19:22 +0200

    improved UI, added code input

--- example-4/static/script.js
+++ example-4/static/script.js
@@ -1,18 +1,18 @@
 var tag = document.createElement("script");
-tag.src = "https://www.gruveo.com/embed-api/";
+tag.src = "https://local.gruveo.com/embed-api/";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var embed;
 // eslint-disable-next-line no-unused-vars
 function onGruveoEmbedAPIReady() {
-  // eslint-disable-next-line no-undef
   embed = new Gruveo.Embed("myembed", {
     width: 680,
     height: 465,
+    responsive: true,
     embedParams: Object.assign({
       color: "63b2de"
-    }, securityParams) // eslint-disable-line no-undef
+    }, securityParams)
   });
 
   embed
@@ -23,7 +23,7 @@ function onGruveoEmbedAPIReady() {
 function onEmbedReady(e) {
   document.getElementById("call-btn").addEventListener("click", function() {
     // Generate a random code and start a video call on that code.
-    var code = Math.random().toString(36).substr(2, 5);
+    var code = document.getElementById("code-input").value || Gruveo.Embed.generateRandomCode();
     embed.call(code, true);
   });
   document.getElementById("end-btn").addEventListener("click", function() {
@@ -32,7 +32,7 @@ function onEmbedReady(e) {
 }
 
 var endTimeout;
-function onEmbedStateChange(e) {     
+function onEmbedStateChange(e) {
   if (e.state == "call") {
     // Set the call to end after 10 seconds.
     endTimeout = setTimeout(function() {
