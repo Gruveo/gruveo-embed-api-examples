b0be9068dc6ec891b8d1867d30a7593a40f86f64
Martin Ždila <martin@gruveo.com>
2017-07-12 14:49:00 +0200

    added logging

--- example-4/static/script.js
+++ example-4/static/script.js
@@ -18,34 +18,63 @@ function onGruveoEmbedAPIReady() {
   });
 
   embed
-    .on('ready', onEmbedReady)
-    .on('stateChange', onEmbedStateChange)
-    .on('requestToSignApiAuthToken', (token) => {
-      fetch('/signer', {
-        method: 'POST',
-        body: token
-      }).then(res => res.text()).then((signature) => {
-        embed.authorize(securityParams.clientId, signature);
-      });
-    });
+    .on('ready', handleEmbedReady)
+    .on('stateChange', handleEmbedStateChange)
+    .on('requestToSignApiAuthToken', handleEmbedRequestToSignApiAuthToken);
 }
 
-function onEmbedReady(e) {
+function handleEmbedReady(e) {
+  log('Ready.');
   document.getElementById('call-btn').addEventListener('click', () => {
     // Generate a random code and start a video call on that code.
     const code = document.getElementById('code-input').value || Gruveo.Embed.generateRandomCode();
+    log(`Calling code "${code}".`);
     embed.call(code, true);
   });
   document.getElementById('end-btn').addEventListener('click', () => embed.end());
 }
 
 let endTimeout;
-function onEmbedStateChange(e) {
+function handleEmbedStateChange(e) {
+  log(`State set to "${e.state}".`);
   if (e.state === 'call') {
     // Set the call to end after 10 seconds.
-    endTimeout = setTimeout(() => embed.end(), 10000);
+    endTimeout = setTimeout(() => {
+      log(`Ending call (timeout).`);
+      embed.end();
+    }, 10000);
   } else if (e.state === 'ready') {
     clearTimeout(endTimeout);
-    alert(`Call duration: ${e.callDuration} s`);
+    log(`Call duration was ${e.callDuration} s.`);
   }
+}
+
+function handleEmbedRequestToSignApiAuthToken(token) {
+  log(`Signing API Auth token "${token}".`);
+  fetch('/signer', {
+    method: 'POST',
+    body: token
+  })
+    .then((res) => {
+      if (res.status === 200) {
+        return res;
+      }
+      throw new Error(`Unexpected server respnse status code "${res.status}".`);
+    })
+    .then(res => res.text())
+    .then((signature) => {
+      log(`API Auth token signature is "${signature}".`);
+      embed.authorize(securityParams.clientId, signature);
+    })
+    .catch(err => {
+      log(`Error signing API Auth token: ${err.message}`);
+      log('Ending call.');
+      embed.end();
+    });
+}
+
+function log(msg) {
+  const logsElement = document.getElementById('logs');
+  logsElement.value += `${logsElement.value.length ? '\n' : ''}> ${msg}`;
+  logsElement.scrollTop = logsElement.scrollHeight;
 }
